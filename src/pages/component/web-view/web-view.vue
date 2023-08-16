6c9ed7ae898fe6e8a9f41d410efbc24c18d49be4
朱立行 <zhulixing@xiaohongshu.com>
2023-08-16 20:32:43 +0800

    fix: 一期样式问题修复

--- src/pages/component/web-view/web-view.vue
+++ src/pages/component/web-view/web-view.vue
@@ -8,7 +8,11 @@
 	export default {
 		data() {
 			return {
-				url: 'https://uniapp.dcloud.io/static/web-view.html'
+				// url: 'https://uniapp.dcloud.io/static/web-view.html'
+				// uniapp的web-view页面中，没有引入小红书的sdk，按钮点击失效
+				// 线上先使用小红书的域名
+				url: 'https://www.xiaohongshu.com/miniapp/miniapp-webview?fullscreen=true&disableDragBack=yes&disableBackForwardNavigationGestures=true'
+
 			}
 		},
 		onLoad(options) {
